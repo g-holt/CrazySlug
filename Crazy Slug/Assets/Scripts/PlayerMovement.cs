d2e9137102b4e60035b5e6ce1d047956c87b0b8f
Grayson Holt <gholtwork@gmail.com>
2022-08-23 15:46:56 -0500

    Prefab + Animation Updates

--- Crazy Slug/Assets/Scripts/PlayerMovement.cs
+++ Crazy Slug/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    Vector2 moveInput;
+    Animator animator;
+    Animation move;
+    Animation idle;
+
+    float moveX;
+    float moveY;
+    string moveAnim;
+    string idleAnim;
+
+    
     void Start()
     {
-        
+        moveAnim = "player_movement";
+        idleAnim = "player_idle";
+        animator.SetBool(idleAnim, true);
+
+
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        Move();
+    }
+
+
+    void Move()
+    {
+        if(moveInput.x == 0)
+        {
+            animator.SetBool(idleAnim, true);
+            return; 
+        }
         
+        animator.SetBool(moveAnim, true);
+       
+        moveX = moveInput.x * moveSpeed * Time.deltaTime;
+
+        transform.Translate(moveX, 0f, 0f, Space.Self);
+    }
+
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
